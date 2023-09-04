af751d97a2d17fa0198c57736699a680a5b2e64b
khushbu <khushbujanbandhu28@gmail.com>
2023-09-04 21:07:29 +0530

    arraylist code added here

--- SampleDemo/src/com/velocity/Sample.java
+++ SampleDemo/src/com/velocity/Sample.java
@@ -1,5 +1,11 @@
 package com.velocity;
 
 public class Sample {
+   public static void main(String[]args) {
+	   for(int i = 1; i <= 5; i++) {
+		   System.out.println("the value of i is " + i);
+	   }
+   }
 
+	
 }
--- SampleDemo/src/com/velocity/Test.java
+++ SampleDemo/src/com/velocity/Test.java
@@ -1,5 +1,13 @@
 package com.velocity;
 
-public class Test {
+import java.util.ArrayList;
 
+public class Test {
+    public static void  main(String[]args) {
+    	ArrayList al = new ArrayList();
+    	al.add(20);
+    	al.add(34);
+    	al.add(67);
+    	System.out.println(al);
+    }
 }
